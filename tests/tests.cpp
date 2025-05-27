a6ee2835bfa8a8b10575df74ee2e483064517897
NeKon69 <nobodqwe@gmail.com>
2025-05-27 22:02:36 +0300

    implemented hub for shared pointers

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,7 +2,20 @@
 // Created by progamers on 5/27/25.
 //
 #include <iostream>
+#include <memory>
+#include "../include/raw_namespace_functions.h"
+
 int main() {
+	raw::make_unique<int>(42);
+	int t = static_cast<int>(3845578384.54);
 	std::cout << "Hello, World!" << std::endl;
+	int ptr[10];
 	return 0;
+}
+
+auto gg(){
+
+	int ptr[10];
+	return ptr;
+
 }
