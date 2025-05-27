07d9418f7878fb5ee0d06b2f23ff4f4151b98fec
NeKon69 <nobodqwe@gmail.com>
2025-05-27 22:03:17 +0300

    implemented base smart ptr class

---
+++ raw_namespace_smart_ptr.h
@@ -0,0 +1,24 @@
+//
+// Created by progamers on 5/27/25.
+//
+
+#ifndef SMARTPOINTERS_RAW_NAMESPACE_SMART_PTR_H
+#define SMARTPOINTERS_RAW_NAMESPACE_SMART_PTR_H
+
+namespace raw {
+
+template<typename T>
+class hub;
+
+template<typename T>
+class smart_ptr_base;
+
+template<typename T>
+class unique_ptr;
+
+template<typename T>
+class shared_ptr;
+
+} // namespace raw
+
+#endif // SMARTPOINTERS_RAW_NAMESPACE_SMART_PTR_H
