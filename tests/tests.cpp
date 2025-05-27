9b2f23d49241a3e5e0d6b23af2c18f4c684c102d
NeKon69 <nobodqwe@gmail.com>
2025-05-27 19:16:08 +0300

    final cleanup

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,8 +2,7 @@
 // Created by progamers on 5/27/25.
 //
 #include <iostream>
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
 }
