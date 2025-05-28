b087fd1a18eb279c0d88e8af6fb8cbb5e96f138a
NeKon69 <nobodqwe@gmail.com>
2025-05-28 18:32:44 +0300

    finished unique_ptr and tests for it

--- include/classes/smart_ptr_base.h
+++ include/classes/smart_ptr_base.h
@@ -5,8 +5,6 @@
 #ifndef SMARTPOINTERS_SMART_PTR_BASE_H
 #define SMARTPOINTERS_SMART_PTR_BASE_H
 
-#include <cstdlib>
-#include <exception>
 #include <iostream>
 
 #include "raw_namespace_smart_ptr.h"
@@ -20,31 +18,14 @@ protected:
 	friend class shared_ptr<T>;
 
 public:
-	constexpr smart_ptr_base() noexcept {}
-	virtual ~smart_ptr_base() noexcept {
-		if constexpr (std::is_array_v<T>) {
-			std::cout << "Deleting array" << std::endl;
-			delete[] ptr;
-		} else {
-			std::cout << "Deleting single object" << std::endl;
-			delete ptr;
-		}
-	}
+	constexpr smart_ptr_base() = default;
+	explicit smart_ptr_base(nullptr_t) noexcept : ptr(nullptr) {}
 
-	explicit smart_ptr_base(T* p) {
-		if (!p) {
-			throw std::bad_alloc();
-		}
+	explicit smart_ptr_base(T* p) noexcept {
 		ptr = p;
 	}
 
-	explicit smart_ptr_base(T&& p) {
-		if (!p) {
-			throw std::bad_alloc();
-		}
-	}
-
-	operator bool() const noexcept {
+	explicit operator bool() const noexcept {
 		return ptr != nullptr;
 	}
 
@@ -53,17 +34,39 @@ public:
 	}
 
 	T& operator*() const noexcept {
-		if (!ptr) {
-			throw std::runtime_error("Dereferencing null pointer");
-		}
 		return *ptr;
 	}
 
 	T* operator->() const noexcept {
-		if (!ptr) {
-			throw std::runtime_error("Dereferencing null pointer");
-		}
 		return ptr;
+	}
+};
+
+template<typename T>
+class smart_ptr_base<T[]> {
+protected:
+	T* ptr = nullptr;
+	friend class unique_ptr<T[]>;
+	friend class shared_ptr<T[]>;
+
+public:
+	constexpr smart_ptr_base() = default;
+	explicit smart_ptr_base(nullptr_t) noexcept : ptr(nullptr) {}
+
+	explicit smart_ptr_base(T* p) noexcept {
+		ptr = p;
+	}
+
+	explicit operator bool() const noexcept {
+		return ptr != nullptr;
+	}
+
+	T* get() const noexcept {
+		return ptr;
+	}
+
+	T& operator[](size_t index) const noexcept {
+		return ptr[index];
 	}
 };
 } // namespace raw
--- include/raw_namespace_functions.h
+++ include/raw_namespace_functions.h
@@ -5,30 +5,31 @@
 #ifndef SMARTPOINTERS_RAW_NAMESPACE_FUNCTIONS_H
 #define SMARTPOINTERS_RAW_NAMESPACE_FUNCTIONS_H
 
-#endif // SMARTPOINTERS_RAW_NAMESPACE_FUNCTIONS_H
-
 #include <shared_mutex>
+#include <type_traits>
 #include <utility>
 
 #include "raw_namespace_smart_ptr.h"
 
 namespace raw {
-template<typename T, typename... Args>
-/**
- * @brief Creates a unique_ptr that manages a single object.
- * @param args Constructor arguments for the new object.
- */
-unique_ptr<T> make_unique(Args... args) {
-	return unique_ptr<T>(new T(args...));
-}
 
 /**
  * @brief Creates a unique_ptr that manages a static array.
  * @param size size of the array.
  */
 template<typename T>
-unique_ptr<T[]> make_unique(size_t size) {
-	return unique_ptr<T[]>(new T[size]());
+std::enable_if_t<std::is_array_v<T>, raw::unique_ptr<T>> make_unique(size_t size) {
+	using element_type = std::remove_extent_t<T>;
+	return raw::unique_ptr<T>(new element_type[size]());
+}
+
+template<typename T, typename... Args>
+/**
+ * @brief Creates a unique_ptr that manages a single object.
+ * @param args Constructor arguments for the new object.
+ */
+std::enable_if_t<!std::is_array_v<T>, raw::unique_ptr<T>> make_unique(Args&&... args) {
+	return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template<typename T, typename... Args>
@@ -36,7 +37,9 @@ template<typename T, typename... Args>
  * @brief Creates a shared_ptr that manages a single object.
  * @param args Constructor arguments for the new object.
  */
-shared_ptr<T> make_shared(Args... args) {
-	return unique_ptr<T>(new T(args...));
+shared_ptr<T> make_shared(Args&&... args) {
+	return shared_ptr<T>(new T(std::forward<Args>(args)...));
 }
 } // namespace raw
+
+#endif // SMARTPOINTERS_RAW_NAMESPACE_FUNCTIONS_H
